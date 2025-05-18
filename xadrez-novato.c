4e1cd17ec3eba16da17ff59ffe97f00f6ad36fd1
Luana Macedo <156150573+Luanamacz@users.noreply.github.com>
2025-05-18 02:35:52 -0300

    xadrez-novato.c

---
+++ xadrez-novato.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+
+    int movimentosTorre = 5;
+    printf("Movimento da Torre (5 casas para a direita):\n");
+    for (int i = 0; i < movimentosTorre; i++) {
+        printf("Direita\n");
+    }
+
+    printf("\n");
+
+    int movimentosBispo = 5;
+    int i = 0;
+    printf("Movimento do Bispo (5 casas na diagonal Cima Direita):\n");
+    while (i < movimentosBispo) {
+        printf("Cima Direita\n");
+        i++;
+    }
+
+    printf("\n");
+
+    int movimentosRainha = 8;
+    int j = 0;
+    printf("Movimento da Rainha (8 casas para a esquerda):\n");
+    do {
+        printf("Esquerda\n");
+        j++;
+    } while (j < movimentosRainha);
+
+    return 0;
+}
