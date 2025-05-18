20852374390dd8b92080de8a13f07903231a2d2e
Luana Macedo <156150573+Luanamacz@users.noreply.github.com>
2025-05-18 02:26:54 -0300

    xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,28 +1,58 @@
 #include <stdio.h>
 
+void moverTorreRecursivo(int casas) {
+    if (casas > 0) {
+        printf("Direita\n");
+        moverTorreRecursivo(casas - 1);
+    }
+}
+
+void moverBispoRecursivo(int casas) {
+    if (casas > 0) {
+        printf("Cima, Direita\n");
+        moverBispoRecursivo(casas - 1);
+    }
+}
+
+
+void moverRainhaRecursivo(int casas) {
+    if (casas > 0) {
+        printf("Esquerda\n");
+        moverRainhaRecursivo(casas - 1);
+    }
+}
+
 int main() {
 
+    printf("Movimento da Torre (Recursivo):\n");
+    int torreCasas = 5;
+    moverTorreRecursivo(torreCasas);
+    printf("\n");
 
-    printf("****Movimentação da Torre!! ****\n");
-    int i =1;
-    while(i <= 5) {
-        printf("Torre: Direita\n");
-        i++;
+    printf("Movimento do Bispo (Recursivo com Loops Aninhados):\n");
+    int bispoCasas = 5;
+    for (int i = 0; i < bispoCasas; i++) {
+        for (int j = 0; j < 1; j++) {
+            printf("Cima, Direita\n");
+        }
     }
+    printf("\n");
+    printf("Movimento da Rainha (Recursivo):\n");
+    int rainhaCasas = 8;
+    moverRainhaRecursivo(rainhaCasas);
+    printf("\n");
+    printf("Movimento do Cavalo (Loops Aninhados Complexos):\n");
+    int movimento_vertical = 2;
+    int movimento_horizontal = 1;
 
-    printf("****Movimentação do Bispo!! ****\n");
-    int j = 1;
-    do {
-        printf("Bispo: Direita\n"); 
-        printf("Bispo: Cima\n");
-        j++;
-    } while (j <= 5);
-
-    printf("****Movimentação da Rainha!! ****\n");
-
-    for (int k = 1; k <= 8; k++) {
-    printf("Rainha: Esquerda\n");
-}
+    for (int v = 0; v < movimento_vertical; v++) {
+        printf("Cima\n");
+      
+        for (int h = 0; h < movimento_horizontal && v == movimento_vertical - 1; h++) {
+            printf("Direita\n");
+        }
+    }
+    printf("\n");
 
     return 0;
 }
